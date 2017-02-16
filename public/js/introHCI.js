6f98a63768663c60e2129c5a13aef546c16e41a5
agrandle <agrandle@ucsd.edu>
2017-02-16 10:24:45 -0800

    lab 6 completion test

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,12 +27,35 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, addProject);
+
+	console.log("/project/" + idNumber);
 }
 
+function addProject(result) {
+	console.log(result);
+	var idNumber = result.id;
+	var projectDetails = '<img src="' + result['image'] + '" class = "detailsImage">' +
+		'<p>' + result['title'] + '</p>' +
+		'<p><small>' + result['date'] + '</small></p>' +
+		'<p>' + result['summary'] + '</p>';
+	$('#project' + idNumber).find('.details').html(projectDetails);
+}
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
+	$.get("/palette", applyPalette);
+}
+
+function applyPalette (result) {
+	var colors = result.colors['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 	console.log("User clicked on color button");
 }
